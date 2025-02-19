ff410f6e7625e20c57979752109ab24e6cffdc3f
Teja3376 <kcteja62892@gmail.com>
2025-02-19 19:22:22 +0530

    logical

---
+++ CoreJava_Programming/src/com/studenttribe/corejava/logicalprograms/Array_n.java
@@ -0,0 +1,18 @@
+package com.studenttribe.corejava.logicalprograms;
+
+import java.util.Arrays;
+
+public class Array_n {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = {2,2,2,1};
+		int n = a.length/2;
+		for (int i=0; i<=n; i++) {
+			
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
